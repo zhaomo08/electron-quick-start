6982d9bf0dab61cd7a71bc5a6f3aecb8c8ef2cd0
SayMeeveTime <zhaomo0823@gmail.com>
2023-06-07 22:19:58 +0800

    finish package

--- main.js
+++ main.js
@@ -8,13 +8,12 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      // preload: path.join(__dirname, 'preload.js')
     }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
-
+  mainWindow.loadURL('https://chat.openai.com/');
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
